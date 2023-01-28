138892b470875bb8d56d476d9661efeb827c7008
najiya817 <najiyanaji817@gmail.com>
2023-01-28 14:57:01 +0530

    28jan

--- pro/account/models.py
+++ pro/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TeacherModel(models.Model):
+#first last phone email qualification
+    first=models.CharField(max_length=100)
+    last=models.CharField(max_length=100)
+    age=models.IntegerField()
+    phone=models.IntegerField()
+    email=models.EmailField()
+    qualification=models.CharField(max_length=100)
+  
--- pro/teacher/models.py
+++ pro/teacher/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class StudentModel(models.Model):
+    first=models.CharField(max_length=100)
+    last=models.CharField(max_length=100)
+    age=models.IntegerField()
+    address=models.CharField(max_length=100)
+    phone=models.IntegerField()
+    email=models.EmailField()
+  
+    
