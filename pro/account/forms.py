8ff5944d75ce02502fc80a65c05cb9b31556d259
najiya817 <najiyanaji817@gmail.com>
2023-02-06 14:22:46 +0530

    6feb

--- pro/account/forms.py
+++ pro/account/forms.py
@@ -8,3 +8,8 @@ class RegForm(UserCreationForm):
     class Meta:
         model=User
         fields=["first_name","last_name","email","username","password1","password2"]
+
+
+class LogForm(forms.Form):
+    uname=forms.CharField(max_length=100,label="enter username",widget=forms.TextInput(attrs={"class":"form-control"}))
+    pswd=forms.CharField(max_length=100,label="enter password",widget=forms.PasswordInput(attrs={"class":"form-control"}))
--- pro/account/views.py
+++ pro/account/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic import View
-from .forms import RegForm
+from .forms import LogForm, RegForm
 from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 class HomeView(View):
@@ -23,6 +24,27 @@ class RegView(View):
         else:
             messages.error(request,"user registration failed !!")
             return render(request,"reg.html",{"form":form_data})
+        
+
+class LogView(View):
+    def get(self,request,*args,**kwargs):
+        f=LogForm()
+        return render(request,"log.html",{"form":f})
+    def post(self,request,*args,**kwargs):
+        form_data=LogForm(data=request.POST)
+        if form_data.is_valid():
+            un=form_data.cleaned_data.get("uname")
+            ps=form_data.cleaned_data.get("pswd")
+            user=authenticate(request,username=un,password=ps)
+            if user:
+                messages.success(request,"login succesfull")
+                return redirect("h")
+            else:
+                messages.error(request,"login failed!!")
+                return redirect("log")
+        else:
+            return render(request,"log.html",{"form":form_data})
+
 
        
 
